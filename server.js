99d55108a246bcba525d230fa30cb0de678dfdf1
Rasmus Nygren <nygrenrasmus5@gmail.com>
2026-03-01 02:07:16 +0100

    test1

--- server.js
+++ server.js
@@ -6,23 +6,76 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static("public"));
 
-// Socket.io
+
+
+const livereload = require("livereload");
+const connectLivereload = require("connect-livereload");
+
+// Starta livereload-server som kollar public-mappen
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(__dirname + "/public");
+
+// Koppla livereload till express
+app.use(connectLivereload());
+
+
+
+app.use(express.static("public")); // mappen public innehåller index.html + AdminIcon.png + ModIcon.png
+
+let users = [];
+
 io.on("connection", (socket) => {
-    console.log("En användare anslöt");
+  console.log("En användare anslöt");
 
-    socket.on("chat message", (msg) => {
-        io.emit("chat message", msg);
+  socket.on("join", (username, password) => {
+    // Sätt status admin/mod
+    socket.username = username;
+    socket.isAdmin = (username === "Admin" && password === "1");
+    socket.isMod = (username === "Mod" && password === "1");
+
+    // Lägg till användare i listan
+    users.push({
+      name: username,
+      isAdmin: socket.isAdmin,
+      isMod: socket.isMod
     });
 
-    socket.on("disconnect", () => {
-        console.log("En användare lämnade");
+    io.emit("updateUsers", users);
+    io.emit("chat message", {
+      name: username,
+      text: "har gått med i chatten",
+      isAdmin: socket.isAdmin,
+      isMod: socket.isMod
     });
+  });
+
+  socket.on("chat message", (msg) => {
+    const user = users.find(u => u.name === socket.username);
+    io.emit("chat message", {
+      name: socket.username,
+      text: msg,
+      isAdmin: user?.isAdmin || false,
+      isMod: user?.isMod || false
+    });
+  });
+
+  socket.on("disconnect", () => {
+    if(socket.username){
+      users = users.filter(u => u.name !== socket.username);
+      io.emit("updateUsers", users);
+      io.emit("chat message", {
+        name: socket.username,
+        text: "lämnade chatten",
+        isAdmin: socket.isAdmin,
+        isMod: socket.isMod
+      });
+    }
+    console.log("En användare lämnade");
+  });
 });
 
-// Render sätter PORT via environment variable
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Servern kör på port ${PORT}`);
+  console.log(`Servern kör på port ${PORT}`);
 });
